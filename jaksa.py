7985abc83802c8e5167b07348a96a90388566cfe
udintofa <muchammad.udin.mustofa@mail.ugm.ac.id>
2024-10-09 13:17:42 +0700

    Add files via upload

---
+++ jaksa.py
@@ -0,0 +1,58 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import time
+import pandas as pd
+import numpy as np
+
+# Fungsi untuk membaca data dari Google Sheets
+def load_data(url):
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    data = conn.read(spreadsheet=url, usecols=[1, 2, 3, 4, 5])
+    return data
+
+# URL Google Sheets
+url = 'https://docs.google.com/spreadsheets/d/1tU0OByLfC0ISXWk2OAhknPnrpSeOX2GtaZFG0DWjwvc/edit?usp=sharing'
+    
+# Memuat dan menampilkan data
+
+data = load_data(url)
+data["Angkatan UGM"] = data["Angkatan UGM"].astype(str)
+data["Angkatan masuk JS"] = data["Angkatan masuk JS"].astype(str)
+
+# Menggunakan reset_index dan mulai indeks dari 1
+data = data.reset_index(drop=True)  # Reset index terlebih dahulu
+data.index = data.index + 1         # Mengubah index menjadi mulai dari 1
+
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Home", "Panitia", "Pendaftaran", "Pendaftar", "Peserta"])
+
+with tab1:
+    st.header("Tentang Kunjungan LDK Nasional")
+    st.write("Kunjungan Lembaga Dakwah Kampus Nasional merupakan kegiatan rutinan jaksa, amin")
+    
+with tab2:
+    st.header("Panitia Kunjungan LDK Nasional")
+    data_diri = [["Nama","Muchammad Udin Mustofa"], ['Panggilan',"Udin"],["Tempat Lahir","Magelang"],["Tanggal Lahir",pd.to_datetime("2003-07-20")],["Alamat","Magelang"]]
+    dd = pd.DataFrame(data_diri)
+    st.write(dd)
+
+with tab3:
+    st.header("Pendaftaran")
+    st.write("Silahkan daftar untuk menjadi peserta LDK Nasional melalui tautan dibawah")
+    # Menambahkan hyperlink
+    st.markdown(
+        """
+        [Daftar Sekarang](https://docs.google.com/forms/d/e/1FAIpQLSdrODYr9QwUqe2_a5o0o6Jd1oem8ktC1j7oY1UGTGwu9PYVhA/viewform?usp=sf_link)
+        """
+    )
+    
+with tab4:
+    st.header("Pendaftar")
+    st.write("Berikut merupakan daftar pendaftar yang sudah mendaftar")
+    # Tombol refresh data tanpa st.experimental_rerun
+    if st.button("Refresh Data"):
+        st.cache_data.clear()  # Menghapus cache agar data terbaru dimuat
+    st.dataframe(data)
+    
+with tab5:
+    st.header("Pendaftar yang diterima menjadi peserta")
+    st.write("Berikut merupakan daftar pendaftar yang diterima menjadi peserta")
