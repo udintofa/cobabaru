24c878e91f81502ce60c9f0cce6a34207800c315
udintofa <muchammad.udin.mustofa@mail.ugm.ac.id>
2024-10-10 11:29:46 +0700

    Update jaksa.py

--- jaksa.py
+++ jaksa.py
@@ -75,7 +75,7 @@ with tab4:
     
 with tab5:
     st.header("Pendaftar yang diterima menjadi peserta")
-    st.write("Berikut merupakan daftar pendaftar yang diterima menjadi peserta")
+    st.write("Berikut merupakan daftar pendaftar yang diterima menjadi peserta (kuota peserta 50 mahasiswa)")
     # Tombol refresh data tanpa st.experimental_rerun
     if st.button("Refresh  Data"):
         st.cache_data.clear()  # Menghapus cache agar data terbaru dimuat
