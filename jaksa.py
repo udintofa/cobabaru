ab493fdd85da0e2338503c218f69c7512ef5475a
udintofa <muchammad.udin.mustofa@mail.ugm.ac.id>
2024-10-10 12:12:52 +0700

    Update jaksa.py

--- jaksa.py
+++ jaksa.py
@@ -41,7 +41,7 @@ st.write('''
 mari bermuhasabah diri, meluruskan niat, ikhlaskan pikiran, dan berusaha menjadi hamba yang baik untuk Allah SWT
 ''')
 
-tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Home", "Panitia", "Pendaftaran", "Pendaftar", "Peserta", "Gambaran Kegiatan"])
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Home", "Panitia", "Pendaftaran", "Pendaftar", "Peserta"])
 
 with tab1:
     st.header("Tentang Kunjungan LDK Nasional")
@@ -64,16 +64,13 @@ with tab3:
         [Daftar Sekarang](https://docs.google.com/forms/d/e/1FAIpQLSdrODYr9QwUqe2_a5o0o6Jd1oem8ktC1j7oY1UGTGwu9PYVhA/viewform?usp=sf_link)
         """
     )
-    
-with tab4:
-    st.header("Pendaftar")
+
     st.write("Berikut merupakan daftar pendaftar yang sudah mendaftar")
     # Tombol refresh data tanpa st.experimental_rerun
     if st.button("Refresh Data"):
         st.cache_data.clear()  # Menghapus cache agar data terbaru dimuat
     st.dataframe(data)
-    
-with tab5:
+
     st.header("Pendaftar yang diterima menjadi peserta")
     st.write("Berikut merupakan daftar pendaftar yang diterima menjadi peserta (kuota peserta 50 mahasiswa)")
     # Tombol refresh data tanpa st.experimental_rerun
@@ -82,7 +79,12 @@ with tab5:
      # Tombol refresh data tanpa st.experimental_rerun
     st.dataframe(data2)
 
-with tab6:
+with tab4:
+    st.header("Pembayaran")
+    st.write("Berikut panduan pembayaran")
+
+
+with tab5:
     st.header("Gambaran Kegiatan")
     st.write("Kegiatan diawali dengan memperkenalkan masing-masing LDK, dilanjutkan dengan sharing-sharing antar LDK. Rundown kegiatan adalah sebagai berikut:")
     rd = [
