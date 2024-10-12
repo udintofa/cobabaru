c976c1d9afcf7ab979f7b5fc2176ceb9679d8a41
udintofa <muchammad.udin.mustofa@mail.ugm.ac.id>
2024-10-12 19:08:13 +0700

    Update jaksa.py

--- jaksa.py
+++ jaksa.py
@@ -51,20 +51,19 @@ data2 = data2.reset_index(drop=True)  # Reset index terlebih dahulu
 data2.index = data2.index + 1         # Mengubah index menjadi mulai dari 1
 
 st.write('''
-# :sunglasses: Selamat Datang :sunglasses:
-### di Laman Departemen Jaksa :smile:
+# :raised_hand_with_fingers_splayed: Selamat Datang :raised_hand_with_fingers_splayed:
+### di Laman Departemen Jaksa :blush::pray:
 mari bermuhasabah diri, meluruskan niat, ikhlaskan pikiran, dan berusaha menjadi hamba yang baik untuk Allah SWT
 ''')
 
-tab1, tab2, tab3, tab4, tab5 = st.tabs(["Home", "Panitia", "Pendaftaran", "Peserta", "Gambaran Kegiatan"])
+tab1, tab3, tab5, tab6 = st.tabs(["Home", "Daftar & Bayar", "Kegiatan", "Keperluan"])
 
 with tab1:
     st.header("Tentang Kunjungan LDK Nasional")
-    st.write("#### JS gass ke Unair dan UB besok :rocket::rocket:")
+    st.write("#### JS gass ke Unair dan UB besok :bus::rocket::bus::rocket:")
     st.write("Kunjungan LDKN merupakan salah satu kegiatan Departemen Jaringan dan Kerjasama yang di mana dilakukan dalam rangka mempererat silaturahmi antar Lembaga dengan berkunjung ke Lembaga lain untuk saling bertukar informasi seputar strategi yang dilakukan lembaga, metode, kaderisasi dan lain-lain yang bermanfaat untuk kemajuan lembaga serta memahami bagaimana struktur atau kondisi umum lembaga yang bisa diambil dan diterapkan di lembaganya.")  
     st.write("Kunjungan LDK Nasional akan dilaksanakan pada hari Jumat (8 Nov 2024) malam sampai dengan hari Senin (11 Nov 2024) pagi. Kunjungan akan dilaksanakan ke LDK UKMKI Unair terlebih dahulu pada Sabtu (9 Nov 2024) dilanjutkan berkunjung ke LDK UAKI UB pada Minggu (10 Nov 2024).")
-
-with tab2:
+    
     st.header("Panitia Kunjungan LDK Nasional")
     data_diri = [
         ["Penanggung Jawab","Muchammad Udin Mustofa"],
@@ -74,9 +73,13 @@ with tab2:
         ['Perlengkapan', 'Diva Aisya'],
         ['Media','Arif Taufik'],
         ['Acara', 'Fathiya Al-Khansa'],
+        ['Konsumsi', 'Anisa Nurliana']
         
     ]
-    dd = pd.DataFrame(data_diri)
+    dd = pd.DataFrame(data_diri, columns=["Nama Panita", "Jabatan"])
+    # Menggunakan reset_index dan mulai indeks dari 1
+    dd = dd.reset_index(drop=True)  # Reset index terlebih dahulu
+    dd.index = dd.index + 1         # Mengubah index menjadi mulai dari 1
     st.write(dd)
 
 with tab3:
@@ -95,7 +98,6 @@ with tab3:
         st.cache_data.clear()  # Menghapus cache agar data terbaru dimuat
     st.dataframe(data)
 
-with tab4:
     st.header("Pembayaran")
     st.write("Silahkan bagi pendaftar yang sudah menjadi peserta bisa melakukan pembayaran sebesar Rp225.ooo ke rekening Mandiri dengan nomor rekening 1370021364068 atas nama YESYAILLA ABZANI ALFATH."+
             " Bagi peserta yang dalam 1 pekan setelah ditetapkan menjadi peserta belum membayar akan dibatalkan menjadi peserta dan akan digantikan oleh pendaftar lain. Bagi yang ada kendala bisa hubungi cp yang ada.")
@@ -105,14 +107,29 @@ with tab4:
     st.markdown("[Akhwat](http://wa.me/+6287732661779)")
     st.write("Berikut merupakan daftar pendaftar yang diterima menjadi peserta (kuota peserta 50 mahasiswa)")
     # Tombol refresh data tanpa st.experimental_rerun
-    if st.button("Refresh  Data"):
+    if st.button("Refresh_Data"):
         st.cache_data.clear()  # Menghapus cache agar data terbaru dimuat
      # Tombol refresh data tanpa st.experimental_rerun
     st.dataframe(data2)
 
+# with tab4:
+#     st.header("Pembayaran")
+#     st.write("Silahkan bagi pendaftar yang sudah menjadi peserta bisa melakukan pembayaran sebesar Rp225.ooo ke rekening Mandiri dengan nomor rekening 1370021364068 atas nama YESYAILLA ABZANI ALFATH."+
+#             " Bagi peserta yang dalam 1 pekan setelah ditetapkan menjadi peserta belum membayar akan dibatalkan menjadi peserta dan akan digantikan oleh pendaftar lain. Bagi yang ada kendala bisa hubungi cp yang ada.")
+#     st.write("Dana iuran akan digunakan untuk membayar administrasi seperti sewa bus, bayar tol, serta konsumsi peserta.")
+#     st.write("Bagi yang telah membayar, silahkan konfirmasi ke:")
+#     st.markdown("[Ihwan](http://wa.me/+6285727318940)")
+#     st.markdown("[Akhwat](http://wa.me/+6287732661779)")
+#     st.write("Berikut merupakan daftar pendaftar yang diterima menjadi peserta (kuota peserta 50 mahasiswa)")
+#     # Tombol refresh data tanpa st.experimental_rerun
+#     if st.button("Refresh  Data"):
+#         st.cache_data.clear()  # Menghapus cache agar data terbaru dimuat
+#      # Tombol refresh data tanpa st.experimental_rerun
+#     st.dataframe(data2)
+
 
 with tab5:
-    st.header("Gambaran Kegiatan")
+    st.header("Kegiatan")
     st.write("Kegiatan diawali dengan memperkenalkan masing-masing LDK, dilanjutkan dengan sharing-sharing antar LDK. Rundown kegiatan adalah sebagai berikut:")
     rd = [
             ['19.00-20.00', 1, 'kumpul peserta', 'Sekre'],
@@ -137,3 +154,23 @@ with tab5:
     # Menambahkan nama kolom saat membuat DataFrame
     rundown = pd.DataFrame(rd, columns=['Waktu', 'Durasi (jam)', 'Kegiatan', 'Lokasi'])
     st.write(rundown)
+
+
+
+with tab6:
+    st.header("Keperluan Peserta")
+    st.write("Berikut merupakan keperluan peserta yang wajib atau diutamakan untuk dibawa:")
+    st.write('''
+                1. DC hari pertama yaitu kemeja putih
+                2. DC hari kedua yaitu kemeja batik
+                3. Jas Almamater UGM
+                4. Baju ganti
+                5. Alat Solat
+                6. Tumbler
+                7. Sepatu
+                8. Obat-obatan pribadi
+                9. Bantal  (opsional)
+                10. Selimut (opsional)
+                11. Jaket (opsional)
+                12. Peralatan pribadi (opsional)
+            ''')
